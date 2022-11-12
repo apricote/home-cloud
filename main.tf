66391e6224cb135289c4823eec9b68e5f4f94fbb
Julian Tölle <julian.toelle97@gmail.com>
2022-11-12 13:25:36 +0100

    feat: remove old home-cloud cluster

--- main.tf
+++ main.tf
@@ -2,26 +2,6 @@ locals {
   cluster_name = "home-cloud"
 }
 
-module "k3s_cluster" {
-  source = "./k3s_cluster"
-
-  name                    = local.cluster_name
-  server_image            = "ubuntu-18.04"
-  server_location         = "nbg1"
-  control_server_type     = "cx21"
-  compute_server_type     = "cx21"
-  compute_count           = 1
-  domain                  = "c.apricote.de"
-  letsencrypt_email       = "julian.toelle97+le@gmail.com"
-  ssh_keys                = [file("./keys/id_terraform")]
-  hcloud_csi_driver_token = var.hcloud_csi_driver_token
-  flux_git_url            = "git@github.com:apricote/home-cloud-flux"
-
-  providers = {
-    hcloud = hcloud
-  }
-}
-
 module "k3s_cluster_v2" {
   source = "./k3s_cluster_v2"
 
@@ -46,7 +26,7 @@ module "k3s_cluster_v2" {
   branch                          = "main"
   repository_visibility           = "private"
   target_path                     = ""
-  flux_version                    = "v0.31.1"
+  flux_version                    = "v0.36.0"
 
   providers = {
     hcloud = hcloud
--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "cluster_public_ip" {
-  value = module.k3s_cluster.server_public_ip
-}
-
-output "cluster_name" {
-  value = local.cluster_name
-}
-
 output "summary" {
   value = module.k3s_cluster_v2.summary
 }
