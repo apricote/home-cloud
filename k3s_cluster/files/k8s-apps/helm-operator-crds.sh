ad3ba9d059ef85dd07594319dc32832cc5326cc2
Julian Tölle <julian.toelle97@gmail.com>
2022-12-17 16:44:49 +0100

    chore: remove shut-down cluster manifests

--- k3s_cluster/files/k8s-apps/helm-operator-crds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-MANIFEST_FILE=https://raw.githubusercontent.com/fluxcd/helm-operator/${version}/deploy/crds.yaml
-K3S_MANIFEST_FOLDER=${k3s_manifest_folder}
-
-curl -sfL $MANIFEST_FILE > $K3S_MANIFEST_FOLDER/helm-operator-crds.yml
--- k3s_cluster/output.tf
+++
@@ -1,3 +0,0 @@
-output server_public_ip {
-  value = hcloud_floating_ip.server.ip_address
-}
